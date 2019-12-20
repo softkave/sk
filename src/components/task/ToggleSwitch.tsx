81f6b997334b09f7f512b142ea61bc48e818ce4c
Abayomi Akintomide <ywordk@gmail.com>
2019-12-20 01:13:07 -0500

    implementing auto assigning a user on toggle task

--- src/components/task/ToggleSwitch.tsx
+++ src/components/task/ToggleSwitch.tsx
@@ -2,18 +2,12 @@ import { Icon, Switch } from "antd";
 import React from "react";
 import { useDispatch, useSelector, useStore } from "react-redux";
 import { IBlock } from "../../models/block/block";
-import {
-  getTaskCompletionData,
-  getUserTaskCollaborator
-} from "../../models/block/utils";
+import { getTaskCompletionData } from "../../models/block/utils";
 import toggleTaskOperationFunc from "../../redux/operations/block/toggleTask";
 import { toggleTaskOperationID } from "../../redux/operations/operationIDs";
 import { getSignedInUserRequired } from "../../redux/session/selectors";
 import useOperation from "../hooks/useOperation";
-import Loading from "../Loading";
 import StyledContainer from "../styled/Container";
-
-const StyledIcon = StyledContainer.withComponent(Icon);
 
 export interface IToggleSwitchProps {
   task: IBlock;
@@ -29,15 +23,9 @@ const ToggleSwitch: React.FC<IToggleSwitchProps> = props => {
   });
   const store = useStore();
   const dispatch = useDispatch();
-  // const userTaskCollaboratorData = getUserTaskCollaborator(task, user);
-  // const taskCollaborators = task.taskCollaborators || [];
   const completionData = getTaskCompletionData(task, user);
 
   const renderUncheckedChildren = () => {
-    // if (completionData.isCompeleted) {
-    //   return null;
-    // }
-
     if (completionData.type === "individual") {
       return `${completionData.hasCompleted} | ${completionData.total}`;
     }
@@ -59,46 +47,21 @@ const ToggleSwitch: React.FC<IToggleSwitchProps> = props => {
     }
   };
 
-  // const isTaskCompleted = () => {
-  //   const collaborationType =
-  //     task.taskCollaborationType!.collaborationType || "collective";
-
-  //   if (collaborationType === "collective") {
-  //     return !!task.taskCollaborationType!.completedAt;
-  //   } else {
-  //     const hasCompleted = taskCollaborators.filter(
-  //       collaborator => !!collaborator.completedAt
-  //     );
-  //     return userTaskCollaboratorData && !!userTaskCollaboratorData.completedAt
-  //       ? true
-  //       : taskCollaborators.length === hasCompleted.length
-  //       ? true
-  //       : false;
-  //   }
-  // };
-
   const onToggle = () => {
     toggleTaskOperationFunc(store.getState(), dispatch, { user, block: task });
   };
 
-  // if (toggleTaskOperation.isLoading) {
-  //   return <Loading fontSize="16px" />;
-  // }
-
-  // const checked = isTaskCompleted();
-  // const isUserAssigned = !!userTaskCollaboratorData;
-  const l = renderUncheckedChildren();
+  const renderedCompletionInformation = renderUncheckedChildren();
 
   return (
     <StyledContainer s={{ alignItems: "center" }}>
       <Switch
         loading={toggleTaskOperation.isLoading}
         checked={completionData.isCompeleted}
-        // checked={completionData.userHasCompleted || completionData.isCompeleted}
         onChange={onToggle}
-        disabled={disabled || !completionData.userIsAssigned}
-        unCheckedChildren={l}
-        checkedChildren={l}
+        disabled={disabled}
+        unCheckedChildren={renderedCompletionInformation}
+        checkedChildren={renderedCompletionInformation}
       />
       {renderUserHasCompletedMarker()}
     </StyledContainer>
