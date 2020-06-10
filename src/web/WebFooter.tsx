9c9484d4af45bb7512416dda9664aa3d5431ae5c
Abayomi Akintomide <ywordk@gmail.com>
2020-06-10 02:28:27 -0400

    added email to the website footer

--- src/web/WebFooter.tsx
+++ src/web/WebFooter.tsx
@@ -1,11 +1,31 @@
+import MailOutlined from "@ant-design/icons/MailOutlined";
 import styled from "@emotion/styled";
 import React from "react";
+import StyledContainer from "../components/styled/Container";
 
 const WebFooter: React.SFC<{}> = () => {
   return (
     <StyledWebFooter>
-      &copy;&nbsp;&nbsp;-&nbsp;&nbsp;Softkave&nbsp;&nbsp;- &nbsp;&nbsp;
-      {new Date().getFullYear()}
+      <StyledContainer
+        s={{ width: "100%", flex: 1, maxWidth: "750px", margin: "auto" }}
+      >
+        <StyledContainer
+          s={{
+            flex: 1,
+          }}
+        >
+          &copy;&nbsp;&nbsp;-&nbsp;&nbsp;Softkave&nbsp;&nbsp;- &nbsp;&nbsp;
+          {new Date().getFullYear()}
+        </StyledContainer>
+        <StyledContainer>
+          <a
+            href="mailto:abayomi@softkave.com"
+            style={{ color: "rgba(0,0,0,0.65)", fontSize: "16px" }}
+          >
+            <MailOutlined />
+          </a>
+        </StyledContainer>
+      </StyledContainer>
     </StyledWebFooter>
   );
 };
