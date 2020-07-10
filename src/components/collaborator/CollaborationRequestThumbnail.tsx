b67a825f955827b700a39e48959ed3bffe9fa224
Abayomi Akintomide <ywordk@gmail.com>
2020-07-10 05:47:32 -0400

    Cosmetic changes to collaboration requests

--- src/components/collaborator/CollaborationRequestThumbnail.tsx
+++ src/components/collaborator/CollaborationRequestThumbnail.tsx
@@ -1,10 +1,19 @@
 import styled from "@emotion/styled";
-import { Typography } from "antd";
+import { Tag, Typography } from "antd";
 import moment from "moment";
 import React from "react";
-import { INotification } from "../../models/notification/notification";
+import {
+  CollaborationRequestStatusType,
+  INotification,
+} from "../../models/notification/notification";
 import cloneWithWidth from "../styled/cloneWithWidth";
 import StyledContainer from "../styled/Container";
+
+const colorMap = {
+  pending: "#EACA2C",
+  accepted: "#7ED321",
+  declined: "rgb(255, 77, 79)",
+};
 
 export interface ICollaborationRequestThumbnailProps {
   request: INotification;
@@ -21,6 +30,28 @@ const CollaborationRequestThumbnail: React.SFC<ICollaborationRequestThumbnailPro
   const latestStatus = statusHistory[statusHistory.length - 1];
   const expiresAt = moment(request.expiresAt);
   const expired = request.expiresAt && moment().isAfter(expiresAt);
+
+  const renderStatus = () => {
+    if (expired) {
+      return <Tag color={colorMap.declined}>expired</Tag>;
+    }
+
+    switch (latestStatus.status) {
+      case CollaborationRequestStatusType.Accepted:
+        return <Tag color={colorMap.accepted}>accepted</Tag>;
+
+      case CollaborationRequestStatusType.Declined:
+        return <Tag color={colorMap.declined}>declined</Tag>;
+
+      case CollaborationRequestStatusType.Pending:
+        return <Tag color={colorMap.pending}>pending</Tag>;
+
+      case CollaborationRequestStatusType.Revoked:
+        return <Tag color={colorMap.declined}>revoked</Tag>;
+    }
+
+    return null;
+  };
 
   return (
     <StyledContainer
@@ -40,11 +71,7 @@ const CollaborationRequestThumbnail: React.SFC<ICollaborationRequestThumbnailPro
               {moment(request.expiresAt).fromNow()}
             </Typography.Text>
           )}
-          {latestStatus && (
-            <Typography.Text>
-              Status - {expired ? "expired" : latestStatus.status}
-            </Typography.Text>
-          )}
+          <StyledContainer>{renderStatus()}</StyledContainer>
         </StyledRequestDataContainer>,
         { marginLeft: 16 }
       )}
