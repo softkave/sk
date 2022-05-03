9ab6cac07bab52f2bdadb016c783c39cee743cb6
Abayomi Akintomide <ywordk@gmail.com>
2022-05-03 04:05:57 -0400

    Added Express middleware function to route http requests to https

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 5000;
 
+function httpToHttps(req, res, next) {
+  if (req.headers["x-forwarded-proto"] !== "https") {
+    return res.redirect(["https://", req.get("Host"), req.url].join(""));
+  }
+
+  return next();
+}
+
 app.use(httpToHttps);
 app.use(express.static("build"));
 app.get("/*", function (req, res) {
